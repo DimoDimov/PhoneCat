cc851d4683b46e5e99d33eed4574a193dba3c136
DimoDimov <dimo.ivanov.dimov@gmail.com>
2015-11-26 18:26:48 +0000

    added templates

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -16,14 +16,14 @@ describe('PhoneCat App', function() {
         });
 
         it('should filter the phone list as a user types into the search box', function() {
-            expect(phoneList.count()).toBe(3);
+            expect(phoneList.count()).toBe(20);
 
             query.sendKeys('nexus');
             expect(phoneList.count()).toBe(1);
 
             query.clear();
             query.sendKeys('motorola');
-            expect(phoneList.count()).toBe(2);
+            expect(phoneList.count()).toBe(8);
         });
 
         it('should display the current filter value in the title bar', function() {
@@ -69,27 +69,15 @@ describe('PhoneCat App', function() {
                         ]);
                     });
                 });
+        });
 
-
-
-
-            // .then(function (res) {
-            // 	console.log('--------------------');
-
-            // 	console.log(res);
-            // });
-            //});
-            // expect(getNames()).toEqual([
-            //     "Motorola XOOM\u2122 with Wi-Fi",
-            //     "MOTOROLA XOOM\u2122"
-            // ]);
-
-            // element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
-
-            // expect(getNames()).toEqual([
-            //     "MOTOROLA XOOM\u2122",
-            //     "Motorola XOOM\u2122 with Wi-Fi"
-            // ]);
+        it('should render phone specific links', function() {
+            var query = element(by.model('query'));
+            query.sendKeys('nexus');
+            element.all(by.css('.phones li a')).first().click();
+            browser.getLocationAbsUrl().then(function(url) {
+                expect(url).toBe('/phones/nexus-s');
+            });
         });
 
     });
