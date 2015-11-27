55686bd2f515a9d745925f4cd8b3c154d77d2282
DimoDimov <dimo.ivanov.dimov@gmail.com>
2015-11-27 19:42:32 +0000

    partials added

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,12 +1,15 @@
 'use strict';
+var phonecatControllers = angular.module('phonecatControllers', []);
 
-var phonecatApp = angular.module('phonecatApp', []);
-
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$routeParams', '$http', 
+  function($scope, $routeParams, $http) {
     $http.get('phones/phones.json').success(function(data) {
-        $scope.phones = data;
+      $scope.phones = data;
     });
-
-
     $scope.orderProp = 'age';
 }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
+  function($scope, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+  }]);
